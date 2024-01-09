f95fcb3ff3dc04035ed08a678073b82f5df13710
Zhiyuan Zhang <zhiyuan.luckz@gmail.com>
2024-01-09 12:49:58 -0800

    llava served on vllm backend of triton

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
+# Install transformers' main branch with llava support
+# https://huggingface.co/docs/transformers/main/model_doc/llava
+pip install git+https://github.com/huggingface/transformers.git
+
+
+# Pick the llava_devel branch of vllm
+# https://github.com/AzureSilent/vllm/tree/llava_devel
+# https://github.com/vllm-project/vllm/pull/2153
+pip install git+https://github.com/AzureSilent/vllm.git@llava_devel
+
 # Start Triton Server
 tritonserver \
     --model-repository ./model_repository \
